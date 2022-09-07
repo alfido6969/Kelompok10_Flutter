56f1d6dc3e75420bbc5583ee522b7e215adba46f
sleepyboy <106053608+alfido6969@users.noreply.github.com>
2022-09-07 16:41:01 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home:
+        Material(color: Colors.blue, child: Center(child: Text('Hello World'))),
+  ));
+}
